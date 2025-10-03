32b7dafb57daba769a82f729ac78382245eff44b
Carlos Renato <carlos_sdossantos@hotmail.com>
2025-10-03 16:55:07 -0300

    deixando o site responsivo

--- formulario.js
+++ formulario.js
@@ -1,29 +1,42 @@
+const camposFormulario = document.querySelectorAll(".campo");
 const botaoEnviar = document.querySelector(".botao-enviar");
 
-botaoEnviar.addEventListener("click", function (event) {
-    event.preventDefault();
+botaoEnviar.addEventListener("click", (e) => {
+   
+    e.preventDefault();
 
-    const campos = document.querySelectorAll(".campo");
-    const mensagensInvalido = document.querySelectorAll(".invalido");
-
-    campos.forEach(function (campo, indice) {
-        if (campo.value.trim() === '') {
-            // Campo inválido (vazio)
-           mensagensInvalido[indice].classList.add("mostrar")
-           campo.classList.add("semmargem")
-           campo.style.border = "2px solid #F52E2E" 
+    camposFormulario.forEach((input) => {
+        if(input.value){
+            input.classList.add("valido");
+            input.classList.remove("erro");
+            input.nextElementSibling.classList.remove("mostrar");
         } else {
-            // Campo válido (não vazio)
-            if(mensagensInvalido[indice])
-            {
-                mensagensInvalido[indice].classList.remove("mostrar")
-            }
-            campo.classList.remove("semmargem");
-            campo.style.border = "2px solid #0CC22B"
+            input.classList.remove("valido");
+            input.classList.add("erro");
+            input.nextElementSibling.classList.add("mostrar");
         }
-    });
-});
-    
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
